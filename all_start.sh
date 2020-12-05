729179d259098b1a68824b161a3615574b5bec67
underdark456 <31307789+underdark456@users.noreply.github.com>
2020-12-05 20:50:57 +0300

    Update all_start.sh

--- all_start.sh
+++ all_start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-export LOG=/srv/project1.log 
-cd /srv
-echo "--------------- START ---------------" >> $LOG
-echo $(date +"%F %H:%M:%S") >> $LOG
-echo "---- sync start ----" >> $LOG
-make sync >> $LOG
-echo "---- build start ----" >> $LOG
-make build >> $LOG
-echo "---- deploy start ----" >> $LOG
-make deploy ip=$1 >> $LOG
-echo "--------------- END ---------------" >> $LOG
+make sync | tee /home/user/proj1/logs/sync_$(date '+%Y-%m-%d-%H').log
+make build | tee /home/user/proj1/logs/build_$(date '+%Y-%m-%d-%H').log
+make deploy | tee /home/user/proj1/logs/deploy_$(date '+%Y-%m-%d-%H').log
