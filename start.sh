71c4ff4a2d516e5a8581d19978a7313a335d06f6
underdark456 <31307789+underdark456@users.noreply.github.com>
2020-12-07 11:27:55 +0300

    Update start.sh
    |& - added stderr to log
    #!/usr/bin/env bash - more portable

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-make sync | tee /home/user/proj1/logs/sync_$(date '+%Y-%m-%d-%H').log
-make build | tee /home/user/proj1/logs/build_$(date '+%Y-%m-%d-%H').log
-make deploy | tee /home/user/proj1/logs/deploy_$(date '+%Y-%m-%d-%H').log
+make sync |& tee /home/user/proj1/logs/sync_$(date '+%Y-%m-%d-%H').log
+make build |& tee /home/user/proj1/logs/build_$(date '+%Y-%m-%d-%H').log
+make deploy |& tee /home/user/proj1/logs/deploy_$(date '+%Y-%m-%d-%H').log
